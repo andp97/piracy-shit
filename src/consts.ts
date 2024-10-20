1a112d6d6dc9da65c6e67b3c4c4316a2e6b8b660
Andrea Pavone <info@andreapavone.com>
2024-10-20 14:12:45 +0200

    Fix site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Piracy Shit';
-export const SITE_DESCRIPTION = 'L\'archivio di store sul Piracy Shield';
+export const SITE_DESCRIPTION = 'L\'archivio di storie sul Piracy Shield';
