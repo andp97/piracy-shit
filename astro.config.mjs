cdcf8919b37283a3408b9f7ab1c65f19a1d17057
Andrea Pavone <info@andreapavone.com>
2024-10-22 22:00:46 +0200

    fix sitemap configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://piracyshit.it',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+  })],
 });
